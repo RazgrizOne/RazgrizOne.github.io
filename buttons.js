e172e119d30b57dc9d74ba2530eb355f9bc30e01
Jacob Douglass <jacob_douglass@att.net>
2017-11-15 16:54:29 -0500

    prevent undefined dates

--- buttons.js
+++ buttons.js
@@ -4,46 +4,39 @@
 //
 //changeData changes the current data target
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-function changeDate(date){
+function changeDate(date) {
+    console.log(currentdate);
+    currentdate += date;
+
+//prevent undifined date
+    if (currentdate > 12) {
+        currentdate -= 1;
+        return;
+    }
+
+    if (currentdate < 1) {
+        currentdate += 1;
+
+        //alert("invalid month");
+        return;
+    }
     console.log("changeData was called")
-    currentdate += date
-    //checkEnableMinus(currentdate);
-    //checkEnablePlus(currentdate);
+
     datastring = basedata + currentdate.toString();
-    data.eachLayer(function (layer) { 
-        layer.setStyle(calcStyle(layer.feature)) 
-        });
+    data.eachLayer(function (layer) {
+        layer.setStyle(calcStyle(layer.feature))
+    });
 
-    document.querySelector('.content').innerHTML = months[currentdate-1];
+    document.querySelector('.content').innerHTML = months[currentdate - 1];
     buildGraph();
-}
-
-function checkEnableMinus(date){
-    var btn = document.getElementById("minus");
-    if (date < 1){
-        btn.dissabled = true;
-    }
-    else {
-        btn.dissabled = false;
-    }
-}
-
-function checkEnablePlus(date){
-    var btn = document.getElementById("plus");
-    if (date > 12){
-        btn.dissabled = true;
-    }
-    else{
-        btn.dissabled = false;
-    }
 }
 
 //type - String: the new base data type
 //
 //changeBaseData changes the data you are working with.
-function changeBaseData(DataIdentifier){
+function changeBaseData(DataIdentifier) {
     console.log("changeBaseData was called")
-    data.eachLayer(function (layer) { 
-        layer.setStyle(calcStyle(layer.feature)) 
-        });
+    data.eachLayer(function (layer) {
+        layer.setStyle(calcStyle(layer.feature))
+    });
 }
