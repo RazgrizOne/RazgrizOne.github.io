069522676572cb0782f9577bbf76b9f1c4db2960
RazgrizOne <amasw87@gmail.com>
2017-12-01 22:25:54 -0500

    Chart Styling
    A long hopeless attempt at wrangling chartjs.
    
    Apparently they are well aware that it needs better documentation and it is coming in a big update later.

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 Chart.defaults.global.responsive = true
-Chart.defaults.global.defaultColor = Color({ h: 0, s: 80, l: 50 }).toCSS();
+//Chart.defaults.global.defaultColor = Color({ h: 0, s: 80, l: 50 }).toCSS();
 Chart.defaults.global.defaultFontColor = 'black';
 Chart.defaults.global.legend.display = false;
 Chart.defaults.global.barPercentage = 1;
@@ -10,20 +10,20 @@ var chart
 
 function sortData(Countries, data, color) {
 
-//1) combine the arrays:
-var list = [];
-for (var j = 0; j < Countries.length; j++) 
-    list.push({'name': Countries[j], 'value': data[j], 'color': color[j]});
+    //1) combine the arrays:
+    var list = [];
+    for (var j = 0; j < Countries.length; j++)
+        list.push({ 'name': Countries[j], 'value': data[j], 'color': color[j] });
 
-//2) sort:
-list.sort(function(a, b) {
-    return ((a.value < b.value) ? -1 : ((a.value == b.value) ? 0 : 1));
-    //Sort could be modified to, for example, sort on the age 
-    // if the name is the same.
-});
+    //2) sort:
+    list.sort(function (a, b) {
+        return ((a.value < b.value) ? -1 : ((a.value == b.value) ? 0 : 1));
+        //Sort could be modified to, for example, sort on the age 
+        // if the name is the same.
+    });
 
-//3) separate them back out:
-return list
+    //3) separate them back out:
+    return list
 
 }
 
@@ -45,7 +45,7 @@ function graphLoader() {
 
     for (i = 0; i < json_data.features.length; i++) {
         //if(getRelevant(json_data.features[i]) && getName(json_data.features[i]) != "Mexico" && getName(json_data.features[i]) != "Canada" )
-        if (getData(json_data.features[i])>0) {
+        if (getData(json_data.features[i]) > 0) {
             labels.push(getName(json_data.features[i]))
             data_data.push(getData(json_data.features[i]))
             bgcolor.push(getCountryColor(getRank(json_data.features[i])))
@@ -53,16 +53,16 @@ function graphLoader() {
         }
     }
 
-    list = sortData(labels,data_data,bgcolor)
+    list = sortData(labels, data_data, bgcolor)
 
     //Seperate sorted array back out
     for (var k = 0; k < list.length; k++) {
         labels[k] = list[k].name;
         data_data[k] = list[k].value;
         bgcolor[k] = list[k].color;
-    }   
+    }
 
-    
+
 
     var databank = {
         datasets: [{
@@ -104,6 +104,8 @@ function buildGraph() {
 
         // Configuration options go here
 
+        //Good place to see the structure of things
+        //https://jsfiddle.net/94o6vc9w/
         options: {
             tooltips: {
                 //xAlign: 'left',
@@ -113,19 +115,32 @@ function buildGraph() {
                 xAxes: [{
                     ticks: {
                         maxRotation: 0,
-                        max: 500000
+                        max: 500000,
+                        stepSize: 250000,
+                        minor: {
+                            max: 500000,
+                            stepSize: 2500
+                        },
+                        gridLines: {
+                            //Nothing I put in here is taking...
+                            color: 'rgba(255, 255, 255, 0.2)',
+                            lineWidth: 10
+                        }
                     },
                     scaleLabel: {
                         display: true,
                         labelString: 'Inbound Tourists'
                     },
-                    afterFit: function(scaleInstance) {
+                    afterFit: function (scaleInstance) {
                         scaleInstance.width = 300; // sets the width to 100px
                     }
                 }],
                 yAxes: [{
                     barPercentage: 1,
-                    
+                    gridLines: {
+                        zeroLineWidth: 10
+                    }
+
                 }]
             },
             legend: {
